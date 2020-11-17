0736b905d0c27b134781ef296d58cb810db042f0
Kamii Akbari <ungewang@gmail.com>
2020-11-17 19:05:03 +0100

    last edits on front end on grid.

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,7 +1,7 @@
 import React,{useEffect,useState} from 'react';
 import {list} from "../utils/pollService";
 import {useAuthContex} from "../contex/authProvider";
-import { Box,SimpleGrid,Container,Button, Progress, Flex, Spacer } from "@chakra-ui/react"
+import { Box,SimpleGrid,Container,Button, Progress, Flex, Spacer,Grid,Center } from "@chakra-ui/react"
 
 function Home(){
     const [polls, setPolls] = useState(null);
@@ -23,54 +23,51 @@ function Home(){
     },[]);
 
     return(
-        <div>
-                {polls &&
+        <Grid ml="11%"  templateColumns="repeat(5, 1fr)" gap={1}  mt="50px" maxW="200px" maxH="auto">
+        {polls &&
                 polls.map((polls)=>(
-                    <Container key={polls.id} mt="50px" maxW="400px" centerContent>
-                            <Flex direction="rows">
-                                <SimpleGrid columns={[2, null, 3]} spacing="40px">
-                                    <Box boxShadow="base" bg="gray.50" w="350px" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                                        <Box color="gray.500"
-                                             fontWeight="semibold"
-                                             letterSpacing="wide"
-                                             fontSize="xs"
-                                             textTransform="uppercase"
-                                             ml="2" p="3">
-                                            <h3>{polls.questionName}</h3>
-                                            <Box mt="10px" fontSize="sm" color="blue.400">
-                                                <Flex>
-                                                    <Box width="70%">
-                                                        <p>{polls.answerOne}</p>
-                                                        <Progress value={polls.answerOneVal} />
-                                                    </Box>
-                                                    <Spacer/>
-                                                    <Button mt="10px" h="33px">Vote</Button>
-                                                </Flex>
+                    <Center key={polls.id}>
+                        <Container  centerContent >
+                            <Box mt="20px" boxShadow="base" bg="gray.50" w="350px" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+                                <Box color="gray.500"
+                                     fontWeight="semibold"
+                                     letterSpacing="wide"
+                                     fontSize="xs"
+                                     textTransform="uppercase"
+                                     ml="2" p="3">
+                                    <h3>{polls.questionName}</h3>
+                                    <Box mt="10px" fontSize="sm" color="blue.400">
+                                        <Flex>
+                                            <Box width="70%">
+                                                <p>{polls.answerOne}</p>
+                                                <Progress value={polls.answerOneVal} />
                                             </Box>
-                                            <Box mt="10px" fontSize="sm" color="blue.400">
-                                                <Flex>
-                                                    <Box width="70%">
-                                                        <p>{polls.answerTwo}</p>
-                                                        <Progress value={polls.answerTwoVal} />
-                                                    </Box>
-                                                    <Spacer/>
-                                                    <Button mt="10px" h="33px">Vote</Button>
-                                                </Flex>
-
-                                            </Box>
-
-                                            <Box color="green.400" mt="15px" fontSize="10px">
-                                                <p>Created by : {polls.user.email}</p>
-                                                <p>Created : {polls.createdAt}</p>
-                                            </Box>
-                                            <Button mt="10px" colorScheme="blue"><a href={`/poll/${polls.id}`}>Answer Poll</a></Button>
-                                        </Box>
+                                            <Spacer/>
+                                            <Button mt="10px" h="33px">Vote</Button>
+                                        </Flex>
                                     </Box>
-                                </SimpleGrid>
-                            </Flex>
-                    </Container>
+                                    <Box mt="10px" fontSize="sm" color="blue.400">
+                                        <Flex>
+                                            <Box width="70%">
+                                                <p>{polls.answerTwo}</p>
+                                                <Progress value={polls.answerTwoVal} />
+                                            </Box>
+                                            <Spacer/>
+                                            <Button mt="10px" h="33px">Vote</Button>
+                                        </Flex>
+                                    </Box>
+                                    <Box color="green.400" mt="15px" fontSize="10px">
+                                        <p>Created by : {polls.user.email}</p>
+                                        <p>Created : {polls.createdAt}</p>
+                                    </Box>
+                                    <Button mt="10px" colorScheme="blue"><a href={`/poll/${polls.id}`}>Answer Poll</a></Button>
+                                </Box>
+                            </Box>
+                        </Container>
+                    </Center>
+
                 ))}
-        </div>
+        </Grid>
     )
 }
 
